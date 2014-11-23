d54af658ae1ac0f841235efda15fb04b7899891d
Thomas Jakway <tjakway@gmail.com>
2014-11-23 14:53:22 -0500

    now using 32 bit ISAAC

--- src/monty_hall.c
+++ src/monty_hall.c
@@ -6,52 +6,57 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdint.h>
-#include "isaac64.h"
+#include "rand.h"
 
 /*number of times to run the simulation*/
 #define COUNT 100
 
 #define SWITCH TRUE //do we switch doors or not?
 
-ub8 rand_lim(unsigned int limit);
-int get_rand_not(unsigned int limit, unsigned int not[], unsigned int not_size);
+randctx ctx;
 
-int main()
+ub4 rand_lim(ub4 limit);
+ub4 get_rand_not(ub4 limit, ub4 not[], ub4 not_size);
+
+ub4 main()
 {
-	//initialize the cryptographic random number generator
-	randinit(FALSE);
+	ctx.randa=ctx.randb=ctx.randc=(ub4)0;
+	for (ub4 i=0; i<256; ++i) ctx.randrsl[i]=(ub4)0;
+	
+	randinit(&ctx, FALSE); // randinit calls isaac at the end
+	isaac(&ctx);
 	
 	/*num_success = # of correct guesses (got the car)
 	 * num_failure = # of incorrect guesses (got the goat)*/
-	unsigned int num_success=0, num_failure=0;
+	ub4 num_success=0, num_failure=0;
 	
-	for(int i = 0; i < COUNT; i++)
+	for(ub4 i = 0; i < COUNT; i++)
 	{
 		//TRUE = has the car, FALSE = has the goat
-		isaac64();
-		unsigned int doors[] = {0, 0, 0};
+		isaac(&ctx);
+		ub4 doors[] = {0, 0, 0};
 		
-		unsigned int car_index = rand_lim(2); //pick a random door to put the car behind
+		ub4 car_index = rand_lim(2); //pick a random door to put the car behind
 		doors[car_index] = TRUE; 
 		
 		//get a 2nd random number that is not the car--this is the player's pick
 		//get a random number between 0 and 2 (since the problem presents 3 doors)
-		unsigned int not_size = 1;
-		unsigned int not[] = { car_index };
-		int players_pick = get_rand_not(2, not, not_size); 
-		assert(players_pick > -1);
+		ub4 not_size = 1;
+		ub4 not[] = { car_index };
+		ub4 players_pick = get_rand_not(2, not, not_size); 
+		assert(players_pick < 3);
 		
-		//get a random integer that is not the player's choice and is not the car
-		unsigned int not_reveal_size = 2;
-		unsigned int not_reveal[] = { car_index, players_pick };
-		int montys_pick = get_rand_not(2, not_reveal, not_reveal_size);
+		//get a random ub4eger that is not the player's choice and is not the car
+		ub4 not_reveal_size = 2;
+		ub4 not_reveal[] = { car_index, players_pick };
+		ub4 montys_pick = get_rand_not(2, not_reveal, not_reveal_size);
 		
 		//monty revealed the door--do you switch?
 		if(SWITCH)
 		{
 			//switching means you pick whatever monty didn't pick
-			unsigned int new_pick_not_size = 1;
-			unsigned int new_pick_not[] = { montys_pick };
+			ub4 new_pick_not_size = 1;
+			ub4 new_pick_not[] = { montys_pick };
 			players_pick = get_rand_not(2, new_pick_not, new_pick_not_size);
 		}
 		
@@ -81,9 +86,9 @@ int main()
 /**
  * get a random number between 0 and limit inclusive that is not any of the values in not
  */
-int get_rand_not(unsigned int limit, unsigned int not[], unsigned int not_size)
+ub4 get_rand_not(ub4 limit, ub4 not[], ub4 not_size)
 {
-	int pick = -1;
+	ub4 pick = 10000;
 	while(TRUE)
 	{
 		pick = rand_lim(limit);
@@ -91,7 +96,7 @@ int get_rand_not(unsigned int limit, unsigned int not[], unsigned int not_size)
 		//this is an inversion of the usual pattern
 		//instead of checking for membership in an array, check for not membership in an array
 		//if any value matches pick, this pick value is bad--run the loop again
-		for(int i = 0; i < not_size; i++)
+		for(ub4 i = 0; i < not_size; i++)
 		{
 			if(pick == not[i])
 				continue;
@@ -99,25 +104,25 @@ int get_rand_not(unsigned int limit, unsigned int not[], unsigned int not_size)
 	}
 	
 	//pick is an array index and rand_lim should return between 0 and limit inclusive
-	assert(pick > -1);
+	assert(pick < 3);
 }
-
-
 
 /**
  * see http://stackoverflow.com/questions/2999075/generate-a-random-number-within-range
  * generate random number without skew
  */
-ub8 rand_lim(unsigned int limit) {
+ub4 rand_lim(ub4 limit) {
 /* return a random number between 0 and limit inclusive.
  */
-	/*UB8MAXVAL= RAND_MAX*/
-    int divisor = UB8MAXVAL/(limit+1);
-    int retval;
+	
+	isaac(&ctx); // and here it's called a second time before reading from randrsl!
+	/*UB4MAXVAL= RAND_MAX*/
+	double divisor = UB4MAXVAL/(limit+1);
+	ub4 retval;
+	
+	do {
+		retval = rand(&ctx) / divisor; //ISAAC redefines rand so this is OK
+	} while (retval > limit);
 
-    do { 
-        retval = rand() / divisor; //ISAAC redefines rand so this is OK
-    } while (retval > limit);
-
-    return retval;
+	return retval;
 }
